ab4cef006b43f041637b2ae9344a9735755335ad
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-24 20:47:13 +0000

    Finish layout and routes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
+   assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
