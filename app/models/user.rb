ebe3dbb1e918e88cf2442efbde7a769433234a65
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-25 17:22:54 +0000

    Add test for lowercase emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   
   validates :name, 
             presence: true, 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,4 +51,11 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+  
+  test "email addresses should be saved as lower_case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
