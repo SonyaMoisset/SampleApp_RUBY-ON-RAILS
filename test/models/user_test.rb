63cc3a3fb92e8c0b3af67f18a34a615f05489d7a
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-25 08:48:23 +0000

    Add tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,14 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+  
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
 end
