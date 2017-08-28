6133c4680d3812a92e7919fcc9ffaf7faa64c600
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-28 16:38:48 +0000

    Create Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  get     'sessions/new'
+
+  root    'static_pages#home'
   
   resources :users
+    
+  get     '/help',    to: 'static_pages#help'
+  get     '/about',   to: 'static_pages#about'
+  get     '/contact', to: 'static_pages#contact'
   
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get     '/signup',  to: 'users#new'
   
-  get '/signup', to: 'users#new'
+  get     './login',  to: 'sessions#new'
+  post    './login',  to: 'sessions#create'
+  delete  './logout', to: 'sessions#destroy'
 end
