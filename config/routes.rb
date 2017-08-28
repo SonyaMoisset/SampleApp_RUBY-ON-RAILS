689a4dbb7453fed5739471c48ecb9b63d3e7a34d
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-28 16:54:06 +0000

    Add new view for session

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   
   get     '/signup',  to: 'users#new'
   
-  get     './login',  to: 'sessions#new'
-  post    './login',  to: 'sessions#create'
-  delete  './logout', to: 'sessions#destroy'
+  get     '/login',  to: 'sessions#new'
+  post    '/login',  to: 'sessions#create'
+  delete  '/logout', to: 'sessions#destroy'
 end
