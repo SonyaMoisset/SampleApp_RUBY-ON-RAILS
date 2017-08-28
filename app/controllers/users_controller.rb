668a05ff935d6dc4da642496a99c8528f357c62c
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-28 09:50:38 +0000

    Add redirect route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+     redirect to @user
     else
       render 'new'
     end
