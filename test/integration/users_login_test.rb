e3c56b5afefb9dfe299bf98374895663a2f6fa17
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-29 13:09:42 +0000

    Add test for the remember me checkbox

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -42,4 +42,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+  
+  test "login without remembering" do
+    # Log in to set the cookie
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
