7f4865b23ba88cb64aa099fbad9192f2a0880197
SonyaMoisset <sonya.moisset@gmail.com>
2017-08-30 15:27:13 +0000

    Add current_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user,  only: [:edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -45,11 +46,17 @@ class UsersController < ApplicationController
     
     # Before filters
     
-    # Confirm a logged-in user
+    # Confirm a logged-in user.
     def logged_in_user
       unless logged_in?
       flash[:danger] = "Please log in."
       redirect_to login_url
       end
+    end
+    
+    # Confirm the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
