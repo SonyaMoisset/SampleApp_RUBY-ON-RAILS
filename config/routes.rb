2965b808d8485cb959a61dc0ffc8abab8d5d085e
SonyaMoisset <sonya.moisset@gmail.com>
2017-09-02 20:12:19 +0000

    Add a resource for password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get     'sessions/new'
-
   root    'static_pages#home'
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
     
   get     '/help',    to: 'static_pages#help'
   get     '/about',   to: 'static_pages#about'
