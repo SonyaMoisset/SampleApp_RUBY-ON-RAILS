a760addc9e970ed032b77a8fe0ba6ba965544f54
SonyaMoisset <sonya.moisset@gmail.com>
2017-09-03 20:07:56 +0000

    Add test for password reset mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
-  def password_reset
+  def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset"
   end
