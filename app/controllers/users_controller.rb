1fec4544277d66d2f2832913278f3eb41a3d6421
SonyaMoisset <sonya.moisset@gmail.com>
2017-09-04 22:31:19 +0000

    Add authorization to Microposts controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,15 +58,6 @@ class UsersController < ApplicationController
     
     # Before filters
     
-    # Confirm a logged-in user.
-    def logged_in_user
-      unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-      end
-    end
-    
     # Confirm the correct user.
     def correct_user
       @user = User.find(params[:id])
