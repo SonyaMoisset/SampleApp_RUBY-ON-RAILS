3b6128ff41b4f47d2f220de61c79d956a0abad60
SonyaMoisset <sonya.moisset@gmail.com>
2017-09-04 08:09:35 +0000

    Refactor code for build a micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save { email.downcase! }
   before_create :create_activation_digest
