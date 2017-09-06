fecaaa47bf3c3f0c6604215b8136dc42f065c4c5
SonyaMoisset <sonya.moisset@gmail.com>
2017-09-06 08:19:01 +0000

    Add partials for following process

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root    'static_pages#home'
   
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
     
   get     '/help',    to: 'static_pages#help'
   get     '/about',   to: 'static_pages#about'
