56ac12f27ce4875eebe2766ea46a27b0168ec241
SonyaMoisset <sonya.moisset@gmail.com>
2017-09-06 12:56:54 +0000

    Add views for following

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
   root    'static_pages#home'
-  
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
     
   get     '/help',    to: 'static_pages#help'
   get     '/about',   to: 'static_pages#about'
@@ -20,4 +10,14 @@ Rails.application.routes.draw do
   get     '/login',  to: 'sessions#new'
   post    '/login',  to: 'sessions#create'
   delete  '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
